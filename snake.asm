ad0193b443889e9d2bc9b4c40a010a550bde70d1
David Cian <cian_david@outlook.com>
2018-11-27 12:31:35 +0000

    Replace snake.asm

--- snake.asm
+++ snake.asm
@@ -11,16 +11,15 @@
 
 main:
   
-  ;;;;;;;SET STACK HERE
-  call clear_leds
-  call get_input
-  addi a0, zero, 1
-  addi a1, zero, 3
-  call set_pixel
-  addi a0, zero, 5
-  addi a1, zero, 15
-  call set_pixel
-  jmpi end
+  addi sp, sp, LEDS ; stack pointer = LEDS address
+
+game_loop:
+  addi t0, zero, 1 ; t0 = 1
+  ldw t1, BUTTONS + 4 (zero)
+  slli t1, t1, 4
+  and t1, t1, t0
+  bne 
+  jmpi game_loop
 
 ; ---------------------------------------clear_leds
 ; BEGIN:clear_leds
@@ -97,7 +96,7 @@ get_input:
 loop:
   and t2, t2, t5 ; edge_capture & mask
   slli t5, t5, 1 ; mask << 1
-  beq t2, zero, loop ; end loop
+  beq t2, zero, end_get_input ; end loop
 
   beq t2, zero, end_get_input ;if equal to 0
   addi t3, zero, 1
@@ -215,12 +214,13 @@ update_right_tail:
   jmpi end_move_snake 
 
 end_move_snake:
-ret
+  ret
 
 ; END:move_snake
 
 ; ---------------------------------------draw_array
 ; BEGIN:draw_array
+draw_array:
   addi t2, zero, 0 ;x
   addi t3, zero, 0 ;y
   addi t0, zero, 11 ;xlim
@@ -242,14 +242,112 @@ loopx:
   bne t2, t0, loopx
 
 end_draw_array:
-ret
+  ret
 ; END:draw_array
 
+; BEGIN:create_food
+create_food:
+  ldw t0, RANDOM_NUM (zero) ; t0 = random number
+  srli t0, t0, 24 ; t0 = least significant byte
+  blt t0, zero, end_create_food ; if (random < 0) return
+  addi t1, zero, 96 ; t1 = 96, 95 is the end bound of game array
+  bge t0, t1, end_create_food ; if (random > 95) return
 
+  slli t2, t0, 2 ; t2 = 4 * t0
+  ldw t3, 0 (t2) ; t3 = GSA[t0]
+  bne t3, zero, end_create_food ; if (GSA[t0] != 0) return
 
+  addi t4, zero, 5 ; t4 = 5
+  stw t4, 0 (t2) ; GSA[t0] = 5
+
+end_create_food:
+  ret
+; END:create_food
+
+; BEGIN:hit_test
+hit_test:
+  ldw t0, HEAD_X (zero) ; t0 = head_x
+  ldw t1, HEAD_Y (zero) ; t1 = head_y
+  slli t3, t0, 3 ; t3 = t0 * 8
+  add t3, t3, t1 ; t3 = t3 + head_y
+  ldw t4, GSA (t3) ; t4 = GSA[head_x][head_y]
+
+  ;TODO update hit test names to predict move
+
+  addi t5, zero, 1
+  bne t4, t5, left_hit_test
+  addi t5, zero, 2
+  bne t4, t5, up_hit_test
+  addi t5, zero, 3
+  bne t4, t5, down_hit_test
+  addi t5, zero, 4
+  bne t4, t5, right_hit_test
+left_hit_test:
+  addi t0, t0, -1 ; head_x--
+up_hit_test:
+  addi t1, t1, -1 ; head_y--
+down_hit_test:
+  addi t1, t1, 1 ; head_y++
+right_hit_test:
+  addi t0, t0, 1 ; head_x++
+
+  addi t3, zero, 0 ; t3 = 0
+  slli t3, t0, 3 ; t3 = t0 * 8
+  add t3, t3, t1 ; t3 = t3 + head_y
+  ldw t4, GSA (t3) ; t4 = GSA[head_x][head_y] (predicted)
+
+  blt t0, zero, screen_body_collide
+  blt t1, zero, screen_body_collide
+  addi t6, zero, 12
+  addi t7, zero, 8
+  bge t0, t6, screen_body_collide
+  bge t1, t7, screen_body_collide
+
+  ;collision with food
+  addi t0, zero, 5
+  bne t4, t0, food_collide
+  addi t0, zero, 1
+  bne t4, t0, screen_body_collide
+  addi t0, zero, 2
+  bne t4, t0, screen_body_collide
+  addi t0, zero, 3
+  bne t4, t0, screen_body_collide
+  addi t0, zero, 4
+  bne t4, t0, screen_body_collide
+
+food_collide:
+  addi v0, zero, 1
+  jmpi end_hit_test
+screen_body_collide:
+  addi v0, zero, 2
+  jmpi end_hit_test
+
+  addi v0, zero, 0
+
+end_hit_test:
+  ret
+; END:hit_test
+
+; BEGIN:display_score
+display_score:
+
+  ret
+; END:
+
+; BEGIN:restart_game
+restart_game:
+  stw zero, TAIL_X (zero) ; tail_x = 0
+  stw zero, TAIL_Y (zero) ; tail_y = 0
+  stw zero, HEAD_X (zero) ; head_x = 0
+  stw zero, HEAD_Y (zero) ; head_y = 0
+  addi t0, zero, 4 ; t0 = 4
+  stw t0, GSA (zero) ; GSA[0] = 4
+
+  ret
+; END:restart_game
 
 ; ---------------------------------------wait
-; BEGIN: wait
+; BEGIN:wait
 wait:
  ;addi t0, zero, 100000
  ;addi t0, t0, -1
@@ -262,6 +360,18 @@ wait:
 ; ---------------------------------------END
 end:
  break
+
+font_data:
+  .word 0xFC ; 0
+  .word 0x60 ; 1
+  .word 0xDA ; 2
+  .word 0xF2 ; 3
+  .word 0x66 ; 4
+  .word 0xB6 ; 5
+  .word 0xBE ; 6
+  .word 0xE0 ; 7
+  .word 0xFE ; 8
+  .word 0xF6 ; 9
 
 
 
